e85fa5ad8f809c3e86b2f3e54a1accc5d0d45ad6
Yulatun <julialimbekov@gmail.com>
2023-01-18 11:00:01 +0200

    feat: add legend

--- src/App.js
+++ src/App.js
@@ -4,23 +4,32 @@ import { defaultSettings } from './defaultSettings';
 
 function App({ data = [], settings = {} }) {
   return (
-    <div className='main-container-all-scales'>
-      {data.map((item, i) => {
-        let dS = defaultSettings[item.type] || {};
-        let overrideSettings = settings[item.type] || {};
-        let combinedSettings = {
-          ...dS,
-          ...overrideSettings,
-        };
-        return (
-          <ArrowChart
-            key={i}
-            {...combinedSettings}
-            type={item.type}
-            score={item.score}
-          />
-        );
-      })}
+    <div className='chart-container'>
+      <div className='main-container-all-scales'>
+        {data.map((item, i) => {
+          let dS = defaultSettings[item.type] || {};
+          let overrideSettings = settings[item.type] || {};
+          let combinedSettings = {
+            ...dS,
+            ...overrideSettings,
+          };
+          return (
+            <ArrowChart
+              key={i}
+              {...combinedSettings}
+              type={item.type}
+              score={item.score}
+            />
+          );
+        })}
+      </div>
+      <div className='legend'>
+        {' '}
+        <div className='legend-rhombus'  />
+        Your score
+        <div className='legend-benchmark'  />
+        MindLode Benchmark
+      </div>
     </div>
   );
 }
